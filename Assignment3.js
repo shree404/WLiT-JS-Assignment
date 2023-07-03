683b3e8f90c0379eafce7e4c6c00d644221491a3
Shreesha Aatreya <shreeshaaatreya1@gmail.com>
2023-07-03 23:22:52 +0545

    Assignment 3 completed

---
+++ Assignment3.js
@@ -0,0 +1,10 @@
+const cylinder = {
+  radius: 7,
+  height: 7,
+  Area: function() {
+    const pi = Math.PI;
+    const area = pi * Math.pow(this.radius, 2) * this.height;
+    return area.toFixed(3); 
+};
+//for Output
+console.log(`The area of the cylinder is: ${cylinder.Area()}`);
